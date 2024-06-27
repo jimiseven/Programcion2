fff29d1d464c140c9c7e7e968c662d0537c9b9cf
LaptopTarks <tano777864@gmail.com>
2024-06-27 18:03:17 -0400

    sincronizacion y bloqueos

---
+++ EjerThreads/Ejer1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Ejer1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola");
+            Console.WriteLine("hola");
+            Thread.Sleep(5000);//duerme el hilo
+        }
+    }
+}
--- EjerThreads/Ejer1/Program.cs
+++ EjerThreads/Ejer1/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
+using System.Threading;//llamamos labreria +
+
+
+//metodos importantes
+//join para sincronizar termina una tarea y despues empieza con otra
+//block para bloquear
 
 namespace Ejer1
 {
@@ -11,10 +16,20 @@ namespace Ejer1
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(MetodoSaludo);//utiilizamos el metodosaludo para thread
+            t.Start();//iniciamos el hilo
+            t.Join();// hasta que no termine el t no hace t2
+            Thread t2 = new Thread(MetodoSaludo);//utiilizamos el metodosaludo para thread
+            t2.Start();//iniciamos el hilo
+            t2.Join();
             Console.WriteLine("hola");
             Thread.Sleep(5000);//duerme el hilo
             Console.WriteLine("hola");
             Console.WriteLine("hola");
+        }
+        static void MetodoSaludo() {
+            Console.WriteLine("hola desde thread");
+            Console.WriteLine("hola desde thread");
         }
     }
 }
